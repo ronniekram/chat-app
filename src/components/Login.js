63f7875f41b06e5673ac3b0cec3f0a108a713c79
ronniekram <arynn.boniface@gmail.com>
2020-10-10 21:28:55 -0700

    added login view

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,6 +21,41 @@ function Login() {
     e.persist();
     setCreds({...creds, [e.target.name]: e.target.value});
 }
+  const login = (e) => {
+    e.preventDefault();
+    setShowLoading(true);
+    ref.orderByChild('nickname').equalTo(creds.nickname).once('value', snapshot => {
+      if (snapshot.exists()) {
+          localStorage.setItem('nickname', creds.nickname);
+          history.push('/roomlist');
+          setShowLoading(false);
+      } else {
+          const newUser = firebase.database().ref('users/').push();
+          newUser.set(creds);
+          localStorage.setItem('nickname', creds.nickname);
+          history.push('/roomlist');
+          setShowLoading(false);
+      }
+  });
+};
+  return (
+    <div>
+      {showLoading &&
+          <Spinner color="primary" />
+      }
+      <Jumbotron>
+          <Form onSubmit={login}>
+              <FormGroup>
+                  <Label>Nickname</Label>
+                  <Input type="text" name="nickname" id="nickname" placeholder="Enter Your Nickname" value={creds.nickname} onChange={onChange} />
+              </FormGroup>
+              <Button variant="primary" type="submit">
+                  Login
+              </Button>
+          </Form>
+      </Jumbotron>
+    </div>
+  );
 
 }
 
