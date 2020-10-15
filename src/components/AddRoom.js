340ea0807ec7b492c68847110a4c7ff73111cb5c
ronniekram <arynn.boniface@gmail.com>
2020-10-14 21:24:21 -0700

    added save room function

--- src/components/AddRoom.js
+++ src/components/AddRoom.js
@@ -20,6 +20,27 @@ function AddRoom() {
   const [showLoading, setShowLoading] = useState(false);
   const ref = firebase.database().ref('rooms/');
 
+  const save = (e) => {
+    e.preventDefault();
+    setShowLoading(true);
+    ref.orderByChild('roomname').equalTo(room.roomname).once('value', snapshot => {
+        if (snapshot.exists()) {
+            return (
+                <div>
+                    <Alert color="primary">
+                        Room name already exist!
+                    </Alert>
+                </div>
+            );
+        } else {
+            const newRoom = firebase.database().ref('rooms/').push();
+            newRoom.set(room);
+            history.goBack();
+            setShowLoading(false);
+        }
+    });
+};
+
 }
 
 export default AddRoom;
