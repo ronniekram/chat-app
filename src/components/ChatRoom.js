302ec702714457c4ecc1afa08595d2bb1d343b62
ronniekram <arynn.boniface@gmail.com>
2020-10-15 00:10:59 -0700

    function to extract firebase response

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -57,6 +57,18 @@ useEffect(() => {
   fetchData();
 }, [room, roomname]);
 
+const snapshotToArray = (snapshot) => {
+  const returnArr = [];
+
+  snapshot.forEach((childSnapshot) => {
+      const item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+  });
+
+  return returnArr;
+}
+
 }
 
 export default ChatRoom;
